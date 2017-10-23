cb2b983961c6f35d36f5a66eca79112c93758df9
Daniel Miranda <danielkza2@gmail.com>
2017-10-23 00:51:57 -0200

    test: update setup to use pytest-cov

--- test.sh
+++ test.sh
@@ -3,6 +3,4 @@
 set -e
 
 flake8 ebs_snatcher
-coverage erase
-coverage run --source ebs_snatcher -m py.test
-coverage report --include='ebs_snatcher/**' --omit='ebs_snatcher/test/**'
+pytest --cov=ebs_snatcher ebs_snatcher/test
